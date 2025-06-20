cdcbf85880c9c00e184e9bb6eba4e86e61469f49
Kusillo <colectivoenrojo@gmail.com>
2025-06-20 03:19:32 -0400

    Add Django REST framework integration with serializers and views for Menu and Booking

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuView.as_view()),
+    path('booking/', views.BookingView.as_view()),
+    path('booking/<int:pk>', views.SingleBookingView.as_view()),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from rest_framework import generics
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
@@ -6,3 +9,18 @@ def index(request):
 
 def home(request):
     return render(request, 'index.html')
+
+class MenuView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+class SingleMenuView(generics.RetrieveAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class SingleBookingView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
