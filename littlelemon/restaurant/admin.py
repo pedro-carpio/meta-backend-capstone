9165d6a2ff28949a76c2142dffc212417df465e3
Kusillo <colectivoenrojo@gmail.com>
2025-06-20 03:01:39 -0400

    Add restaurant app models, admin registration, and initial migration

--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Menu, Booking
+
 # Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    search_fields = ['name']
+    ordering = ['name']
+
+@admin.register(Booking)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'guest_number', 'comment']
+    search_fields = ['first_name', 'last_name']
+    ordering = ['first_name']
+
