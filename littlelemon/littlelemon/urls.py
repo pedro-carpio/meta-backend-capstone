c7050900d15086563799e33d5d59c426942ec435
Kusillo <colectivoenrojo@gmail.com>
2025-06-20 03:48:30 -0400

    Add authentication support with Djoser and Token Authentication

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from restaurant import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'tables', views.BookingViewSet)
@@ -28,4 +29,7 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/restaurant/', permanent=False)),
     path('restaurant/', include('restaurant.urls')),
     path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('api-token-auth/', obtain_auth_token)
 ]
